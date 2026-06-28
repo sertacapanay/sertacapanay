4aac67b7c62fc225f1f157a81e2bee25cae5eeea
Sertac Apanay <sertac@hotmail.com>
2026-06-29 01:30:10 +0300

    fix: add ComingSoon to Filament panel middleware

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -51,6 +51,7 @@ class AdminPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
+                \App\Http\Middleware\ComingSoon::class,
             ])
             ->authMiddleware([
                 Authenticate::class,
